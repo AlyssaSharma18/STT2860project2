7ccbe811101bdf97d40217543ec44487cadacea1
AlyssaSharma18 <alyssasharma18@gmail.com>
2021-12-07 13:55:02 -0500

    commit3

--- docsscripts/read_streamingcontent_xlsx.R
+++ docsscripts/read_streamingcontent_xlsx.R
@@ -1,28 +1,38 @@
+library(readxl)
+library(tidyverse)
+library(XLConnect)
+setwd("~/STT2860project2/dataraw")
 # reads two datasets from the raw data file streamingcontent.xlsx
 # writes the datasets to individual .rds files and an .Rdata file
 # original data is in the raw folder, new in the processed folder
 
+#save(file = "streamingcontent.xlsx")
+
 
 # view the names of the worksheets that are in streamingcontent.xlsx
 
-ERASE THIS AND ENTER THE PROPER CODE
-
+#wb <- loadWorkbook("streamingcontent.xlsx")
+#names(wb)
 
 # read in the first sheet of streamingcontent.xlsx and name it dplus
 
-ERASE THIS AND ENTER THE PROPER CODE
+dplus <- read_xlsx("streamingcontent.xlsx", sheet = 1)
+
 
 
 # read in the second sheet of streamingcontent.xlsx and name it strmtv
 
-ERASE THIS AND ENTER THE PROPER CODE
+strmtv <- read_xlsx("streamingcontent.xlsx", sheet = 2)
 
 
 # write the datasets to two .rds files named dplus.rds and strmtv.rds
 
-ERASE THIS AND ENTER THE PROPER CODE
-
+saveRDS(dplus, file = "dplus.rds")
+load("dplus.rds")
+saveRDS(strmtv, file = "strmtv.rds")
+load("strmtv.rds")
 
 # write the datasets to an .Rdata file called streamingcontent.Rdata
 
-ERASE THIS AND ENTER THE PROPER CODE
+save(dplus, strmtv , file = "streamingcontent.Rdata")
+
